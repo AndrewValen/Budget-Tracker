c3d84f903f279cfea6af74d4701d6ada7ebcb671
Andrew Valenzuela <andrewvalenzuela@Andrews-Mac-mini.local>
2025-09-26 10:18:46 -0700

    completed the budget tracker problem with 2 decimal places

--- budget_tracker.py
+++ budget_tracker.py
@@ -12,6 +12,11 @@ entertainment_expenses = float(input("Enter your entertainment expenses: "))
 # Adding all the numbers into a print() 
 total_expenses = food_expenses + rent_expenses + entertainment_expenses
 print("Total monthly expenses: ", total_expenses)
-#subtracting expenses from monthly budget
+#subtracting expenses from monthly budget to get remainder 
 remaining_budget = total_monthly_budget - total_expenses
 print("Remaining budget: ", remaining_budget)
+#Total expenses and remaining budget have been formatted to only take 2 decimal places
+print("--- Budget Summary ---")
+print("Total Monthly Budget:", total_monthly_budget)
+print("Total Expenses: {:.2f}".format(total_expenses))
+print("Remaining Budget: {:.2f}". format(remaining_budget))
